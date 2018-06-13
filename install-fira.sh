ca5fa08e3305114ec4b251e0188cfe2e89420520
stevescott.ca <stevebscott@users.noreply.github.com>
2018-06-13 10:46:49 -0300

    Added Fira Code variant note

--- install-fira.sh
+++ install-fira.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #Updated on 2018-06-13 for FiraSans version 4.3 and to copy both oft and ttf files
+#Note: The variant of Fira Code install does not include ligatures. 
+#      If you want the Fira Code variant with ligatures, follow this: https://github.com/tonsky/FiraCode/wiki/Linux-instructions
 
 wget https://github.com/bBoxType/FiraSans/archive/master.zip
 unzip master.zip
